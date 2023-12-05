d0ff3b58cf58fe3f8fbe1b96d2df4de39a418688
essam <essamheshmey>
2023-12-05 15:27:35 +0200

    Stack

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,8 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
-        long l=factorial(0);
-        System.out.print(l);
-    }
-
-
-      
-
-
-
-    public static long factorial(int i){
-        if (i==1||i==0) {
-            return 1;
-        }else{
-            return i*(factorial(i-1));
+  
         
-        }
     }
-
+    
 }
