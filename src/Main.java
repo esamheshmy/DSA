df0a6e09b120bebb7cf68dd1838ebbfa6933d817
essam <essamheshmey>
2024-02-20 10:37:42 +0200

    Important things have been finished

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import ds.Tree.Tree;
-import ds.nodes.TreeNode;
 
 class Main{
     public static void main(String[]args){
-
-        int n =1000;
-        TreeNode node =new TreeNode(0,null,null);
-        Tree tree = new Tree(node);
-
-        for(int i=0;i<n;i++){tree.add(new TreeNode(i,null,null),tree.node);}
-
-        System.out.print(tree.searsh(80,node));
-
 
 
         }
